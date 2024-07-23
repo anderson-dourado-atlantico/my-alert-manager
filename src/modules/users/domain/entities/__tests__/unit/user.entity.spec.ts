571baa5ea4c23c90c1321b3ce611ce8671d40aeb
Anderson Dourado <anderson_dourado@atlantico.com.br>
2024-07-23 12:58:48 -0300

    UserEntity - testes de unidade dos gets

--- src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -15,23 +15,33 @@ describe('Testes Unitários de UserEntity', () => {
     sut = new UserEntity(props)
   })
 
-  it('deve definir createdAt como a data atual se não estiver definido', () => {
+  it('Testando metodo construtor', () => {
     expect(sut.props.name).toEqual(props.name)
     expect(sut.props.email).toEqual(props.email)
     expect(sut.props.password).toEqual(props.password)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
 
-  // it('não deve alterar createdAt se já estiver definido', () => {
-  //   const existingDate = new Date('2022-01-01')
-  //   const props = {
-  //     name: 'Bob',
-  //     email: 'bob@example.com',
-  //     password: 'senha456',
-  //     createdAt: existingDate,
-  //   }
-  //   sut = new UserEntity(props)
+  it('Testando o Gett do campo name', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
 
-  //   expect(sut.props.createdAt).toBe(existingDate)
-  // })
+  it('Testando o Gett do campo email', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('Testando o Gett do campo password', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Testando o Gett do campo createdAt', () => {
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
 })
