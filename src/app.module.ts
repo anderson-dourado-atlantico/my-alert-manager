97c2c29bb6864b47da2fa68a75cfbb72e1000383
Anderson Dourado <anderson_dourado@atlantico.com.br>
2024-07-22 23:21:05 -0300

    Aplicando configurações de environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 
 @Module({
-    imports: [EnvConfigModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [EnvConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
-import { EnvConfigService } from './env-config.service';
+import { DynamicModule, Module } from '@nestjs/common'
+import { EnvConfigService } from './env-config.service'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
 @Module({
-  providers: [EnvConfigService]
+  providers: [EnvConfigService],
+  exports: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule extends ConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return super.forRoot({
+      ...options,
+      envFilePath: [
+        // join(__dirname, `../../../../environment/.env`),
+        join(__dirname, `../../../../environment/.env.${process.env.NODE_ENV}`),
+      ],
+    })
+  }
+}
